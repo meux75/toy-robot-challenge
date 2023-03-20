ca6fd49754470a3773d51635215c447f613f5923
meux75 <meux.cordova@gmail.com>
2023-03-20 15:23:09 +0800

    added common structures and build

---
+++ lib/common.h
@@ -0,0 +1,43 @@
+#ifndef ROBOT_COMMON_H
+#define ROBOT_COMMON_H
+
+namespace ToyRobot
+{
+    const int MAX_X = 4;
+    const int MIN_X = 0;
+    const int MAX_Y = 4;
+    const int MIN_Y = 0;
+
+    enum class Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    };
+
+    struct Position
+    {
+        int x;
+        int y;
+    };
+
+    enum class CommandType
+    {
+        PLACE,
+        MOVE,
+        LEFT,
+        RIGHT,
+        REPORT
+    };
+
+    struct Command
+    {
+        CommandType type;
+        Position pos;
+        Direction dir;
+    };
+
+}
+
+#endif
